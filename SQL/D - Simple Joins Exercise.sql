566952ee93cfa2250cf7ea7d8f81e76d08e67425
uuuueuphoria <47934058+uuuueuphoria@users.noreply.github.com>
2020-02-25 11:38:50 -0700

    Update D - Simple Joins Exercise.sql

--- SQL/D - Simple Joins Exercise.sql
+++ SQL/D - Simple Joins Exercise.sql
@@ -2,6 +2,11 @@
 USE [A01-School]
 GO
 
+-- we express relationships between tables in our design through FOREIGN KEY constraints.
+-- But those constraints simply check/restrict information that is stored in the Foreign Key column. It does not actually/physically "connect" the tables.
+-- All the tables are independent. That means when we try to pull information from multiple related tables, we have to state the connection between those tables.
+-- That is we have to state how the tables JOIN together.
+
 --1.	Select Student full names and the course ID's they are registered in.
 SELECT  FirstName + ' ' + LastName AS 'Full Name',
         CourseId
@@ -19,6 +24,7 @@ FROM    Student AS S
         ON S.StudentID = R.StudentID -- ON helps us identify MATCHING data
     INNER JOIN Course AS C
         ON R.CourseId = C.CourseId
+--CourseID from multiple table, need to specify which table it comes from
 
 --2.	Select the Staff full names and the Course ID's they teach.
 --      Order the results by the staff name then by the course Id
@@ -33,8 +39,8 @@ ORDER BY 'Staff Full Name', CourseId
 --3.	Select all the Club ID's and the Student full names that are in them
 -- TODO: Student Answer Here...
 SELECT  ClubId, FirstName + ' ' + LastName AS 'Student Full Name'
-FROM    Activity A
-    INNER JOIN Student S ON A.StudentID = S.StudentID
+FROM    Activity AS A
+    INNER JOIN Student AS S ON A.StudentID = S.StudentID
 
 --4.	Select the Student full name, courseID's and marks for studentID 199899200.
 SELECT  S.FirstName + ' ' + S.LastName AS 'Student Name',
@@ -85,6 +91,22 @@ WHERE   S.StudentID = 199912010
 --9. What are the Student Names, courseID's with individual Marks at 80% or higher? Sort the results by course.
 -- TODO: Student Answer Here...
 
+SELECT		FirstName + ' ' + LastName AS 'Student name',
+			CourseID
+FROM		Student AS S
+	INNER JOIN Registration AS R ON S.StudentID=R.StudentID
+WHERE		R.Mark>=80
+ORDER BY	R.CourseId
+
+
 --10. Modify the script from the previous question to show the Course Name along with the ID.
 -- TODO: Student Answer Here...
 
+SELECT		FirstName + ' ' + LastName AS 'Student name',
+			R.CourseID,
+			C.CourseName
+FROM		Student AS S
+	INNER JOIN Registration AS R ON S.StudentID=R.StudentID
+	INNER JOIN Course AS C ON C.CourseId=R.CourseId
+WHERE		R.Mark>=80
+ORDER BY	R.CourseId
