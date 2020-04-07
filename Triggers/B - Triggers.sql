e618ce459944eeb539c450bf4582f352c4fdc04f
uuuueuphoria <47934058+uuuueuphoria@users.noreply.github.com>
2020-04-07 13:22:43 -0600

    Update B - Triggers.sql

--- Triggers/B - Triggers.sql
+++ Triggers/B - Triggers.sql
@@ -4,30 +4,26 @@ GO
 
 -- A. Create a trigger to ensure that an instructor does not teach more than 3 courses in a given semester.
 
-IF EXISTS (SELECT * FROM sys.triggers WHERE object_id = OBJECT_ID(N'[dbo].[Registration_InsertUpdate]'))
-    DROP TRIGGER Registration_InsertUpdate
+
+IF EXISTS (SELECT * FROM sys.triggers WHERE object_id = OBJECT_ID(N'[dbo].[Registration_ProtectPrimaryKey]'))
+    DROP TRIGGER Registration_ProtectPrimaryKey
 GO
 
-CREATE TRIGGER Registration_InsertUpdate
+CREATE TRIGGER Registration_ProtectPrimaryKey
 ON Registration
-
-FOR Insert, Update -- Choose only the DML statement(s) that apply
+FOR Update
 AS
-
-    -- Body of Trigger
-    IF @@ROWCOUNT > 0 -- It's a good idea to see if any rows were affected first
-       AND
-       EXISTS (SELECT R.StaffID FROM Registration R INNER JOIN inserted I ON R.Semester=I.Semester
-				WHERE R.Semester=I.Semester AND R.StaffID=I.StaffID
-               GROUP BY R.StaffID HAVING COUNT(R.CourseId) > 3)
+    IF UPDATE(StudentID) OR UPDATE(CourseID) OR UPDATE(Semester)
     BEGIN
-        -- State why I'm going to abort the changes
-        RAISERROR('Max of 3 classes that a staff can teach', 16, 1)
-        -- "Undo" the changes
+        RAISERROR('Modifications to the composite primary key of Registration are not allowed', 16, 1)
         ROLLBACK TRANSACTION
     END
 RETURN
 GO
+
+
+
+
 
 select * from Registration
 update Registration
@@ -43,30 +39,32 @@ VALUES(199899200, 'DMIT101','2004J', 77.0, 'N',4)
 
 -- B. Create a trigger to ensure that students cannot be added to a course if the course is already full.
 
-IF EXISTS (SELECT * FROM sys.triggers WHERE object_id = OBJECT_ID(N'[dbo].[Registration_Insert]'))
-    DROP TRIGGER Registration_Insert
+
+IF EXISTS (SELECT * FROM sys.triggers WHERE object_id = OBJECT_ID(N'[dbo].[Registration_ClassSizeLimit]'))
+    DROP TRIGGER Registration_ClassSizeLimit
 GO
 
-
-CREATE TRIGGER Registration_Insert
+CREATE TRIGGER Registration_ClassSizeLimit
 ON Registration
 FOR Insert
-AS 
-	IF EXISTS (SELECT C.CourseId FROM Registration R inner join Course C ON R.CourseId=C.CourseId INNER JOIN inserted I ON R.CourseId=I.CourseId
-				WHERE R.CourseId=I.CourseId AND R.Semester=I.Semester
-				GROUP BY MaxStudents, C.CourseId
-				HAVING COUNT(R.StudentID)>=MaxStudents)
-	BEGIN
-		RAISERROR('That class is full', 16, 1)
-		ROLLBACK TRANSACTION
-	END
-	
-	INSERT INTO Waitlist(StudentID, CourseId, Semester, Mark, WithdrawYN, StaffID, DateAdded)
-	SELECT I.StudentID, I.CourseId, I.Semester, I.Mark, I.WithdrawYN, I.StaffID, GETDATE()
-    FROM inserted I
-	
+AS
+    IF  @@ROWCOUNT > 0 AND
+        EXISTS( SELECT  COUNT(R.StudentID)
+                FROM    Course AS C
+                    INNER JOIN Registration AS R ON R.CourseId = C.CourseId
+                    -- Note that the join below is only on the course and semester, because that's how
+                    -- we're interested in grouping to solve this particular question.
+                    INNER JOIN Inserted AS I ON I.CourseId = R.CourseId AND I.Semester = R.Semester
+                WHERE   R.WithdrawYN <> 'Y' -- Don't count those students who have withdrawn                            
+                GROUP BY R.CourseId, R.Semester, C.MaxStudents
+                HAVING  COUNT(R.StudentID) > C.MaxStudents)
+    BEGIN
+        RAISERROR('Student registration cancelled - class is full', 16, 1)
+        ROLLBACK TRANSACTION
+    END
 RETURN
 GO
+
 
 SELECT * FROM Waitlist
 select * from Registration
@@ -78,40 +76,42 @@ VALUES (200122100, 'DMIT259','2004M', 77.00, 'N',4)
 
 -- C. Create a trigger that will add students to a wait list if the course is already full. You should design the WaitList table to accommodate the changes needed for adding a student to the course once space is freed up for the course. Students should be added on a first-come-first-served basis (i.e. - include a timestamp in your WaitList table)
 
-CREATE TABLE Waitlist
-(	StudentID		int				 not null,
-	CourseId		char (7)			not null,
-	Semester		char (5)				not null,
-	Mark			decimal(5,2)		null,
-	WithdrawYN		char (1)			null Constraint DF_GRD		default 'N',
-	StaffID			smallint				null,
-	DateAdded		datetime
+IF EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'WaitList')
+    DROP TABLE WaitList
+GO
+CREATE TABLE WaitList
+(
+    LogID           int  IDENTITY (1,1) NOT NULL CONSTRAINT PK_BalanceOwingLog PRIMARY KEY,
+    StudentID       int                 NOT NULL,
+    CourseID        char(7)             NOT NULL,
+    Semester        char(5)             NOT NULL,
+    AddedOn         datetime            NOT NULL
 )
 GO
 	
-IF EXISTS (SELECT * FROM sys.triggers WHERE object_id = OBJECT_ID(N'[dbo].[OH]'))
-    DROP TRIGGER OH
+ALTER TRIGGER Registration_ClassSizeLimit
+ON Registration
+FOR Insert
+AS
+    IF  @@ROWCOUNT > 0 AND
+        EXISTS( SELECT  COUNT(R.StudentID)
+                FROM    Course AS C
+                    INNER JOIN Registration AS R ON R.CourseId = C.CourseId
+                    -- Note that the join below is only on the course and semester, because that's how
+                    -- we're interested in grouping to solve this particular question.
+                    INNER JOIN Inserted AS I ON I.CourseId = R.CourseId AND I.Semester = R.Semester
+                GROUP BY R.CourseId, R.Semester, C.MaxStudents
+                HAVING  COUNT(R.StudentID) > C.MaxStudents)
+    BEGIN
+        RAISERROR('Student registration cancelled - class is full', 16, 1)
+        INSERT INTO WaitList(StudentID, CourseID, Semester, AddedOn)
+        SELECT StudentID, CourseID, Semester, GETDATE()
+        FROM   inserted
+        ROLLBACK TRANSACTION
+    END
+RETURN
 GO
 
-CREATE TRIGGER OH
-ON Registration
-FOR INSERT -- Choose only the DML statement(s) that apply
-AS
-	BEGIN
-	    INSERT INTO Waitlist(StudentID, CourseId, Semester, Mark, WithdrawYN, StaffID, DateAdded)
-	    SELECT I.StudentID, I.CourseId, I.Semester, I.Mark, I.WithdrawYN, I.StaffID, GETDATE()
-        FROM inserted I
-	END	
-	    IF NOT EXISTS (SELECT C.CourseId FROM Registration R inner join Course C ON R.CourseId=C.CourseId INNER JOIN inserted I ON R.CourseId=I.CourseId
-				WHERE R.CourseId=I.CourseId AND R.Semester=I.Semester
-				GROUP BY MaxStudents, C.CourseId
-				HAVING COUNT(R.StudentID)>=MaxStudents)
-		BEGIN
-		 RAISERROR('CANNOT ADD TO WAITLIST', 16,1)
-		 ROLLBACK TRANSACTION
-		END
- RETURN
-GO
 
 INSERT INTO Registration(StudentID, CourseId, Semester, Mark, WithdrawYN, StaffID)
 VALUES (200688700, 'DMIT101','2000S', 77.00, 'N',1)
@@ -131,8 +131,8 @@ ON Registration
 FOR DELETE
 AS	
 	BEGIN
-		INSERT INTO Registration(StudentID, CourseId, Semester, Mark, WithdrawYN, StaffID)
-		SELECT W.StudentID, W.CourseId, W.Semester, W.Mark, W.Mark, W.StaffID FROM Waitlist W WHERE DateAdded<= ALL (SELECT DateAdded FROM Waitlist)
+		INSERT INTO Registration(StudentID, CourseId, Semester, StaffID)
+		SELECT StudentID, CourseID, Semester, (select StaffID from Registration R inner join WaitList W on W.CourseID=R.CourseId and w.Semester=r.Semester) FROM Waitlist W WHERE AddedOn= ALL (SELECT AddedOn FROM Waitlist)
 		IF @@ERROR<>0
 			BEGIN
 				RAISERROR('Cannot register new student', 16, 1)
